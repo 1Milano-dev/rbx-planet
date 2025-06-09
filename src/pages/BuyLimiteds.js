09805f813b9be849b427cf4b209209edfce89f56
Majorka <4eh1905@gmail.com>
2025-06-09 23:04:23 +0100

    Add BuyLimiteds page to resolve Vercel build error

---
+++ src/pages/BuyLimiteds.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BuyLimiteds() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', color: '#e0e0e0' }}>
+      <h2>Купить Limiteds</h2>
+      <p>Эта страница находится в разработке. Скоро здесь появятся лимитированные предметы!</p>
+    </div>
+  );
+}
+
+export default BuyLimiteds; 

0d2b3694443d3803432e10969c16562e43c49661
Majorka <4eh1905@gmail.com>
2025-06-10 08:10:50 +0100

    Add AddBalance page to resolve Vercel build error

---
+++ src/pages/AddBalance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddBalance = () => {
+  return (
+    <div>
+      <h1>Add Balance</h1>
+      <p>This is the Add Balance page.</p>
+    </div>
+  );
+};
+
+export default AddBalance; 
